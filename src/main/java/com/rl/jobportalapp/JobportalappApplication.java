3b2d43672cfb989f8204342ff0d0c2926c6e6868
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2026-02-04 10:08:41 +0530

    initial commit

---
+++ src/main/java/com/rl/jobportalapp/JobportalappApplication.java
@@ -0,0 +1,13 @@
+package com.rl.jobportalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportalappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rl/jobportalapp/JobportalappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rl.jobportalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
